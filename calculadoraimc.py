321006e0ec3dce7923050e68c3ea673c7a9b1628
DaniGut721 <daniel.gutierrez-s@uniminuto.edu.co>
2024-08-13 20:43:28 -0500

    Pequeños cambios

--- calculadoraimc.py
+++ calculadoraimc.py
@@ -1,12 +1,12 @@
 print("Bienvenido a la calculadora")
-edad= int(input("Por favor ingrese su edad: "))
 peso= int(input("por fa vor ingrese su peso: "))
 estatura =int( input("ingrese su estatura en cm: "))
 imc = peso/(estatura**2)
+print("-----------------------")
 if imc < 18.5 :
     print("bajo Peso")
 elif imc > 18.5 and imc < 24.9:
     print("normal")
 else:
     print("sobrepeso")
-
+print("-----------------------")
